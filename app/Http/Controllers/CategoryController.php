a995db10423d141369e4751c1933d180327fa28b
Administrator <michel@enezaeducation.com>
2020-06-25 19:08:15 +0000

    Making api test

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Category::all()->toArray());
     }
 
     /**
@@ -26,60 +26,13 @@ class CategoryController extends Controller
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        return response()->json(['status' => true, 'message' => 'Category Created']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Category $category)
     {
-        //
+        return response()->json(['status' => true, 'message' => 'Category Deleted']);
     }
 }
---
+++ tests/Unit/APITest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class APITest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCreation(){
+
+        $response = $this->json('POST', '/api/register', [
+            'name' => 'dolo',
+            'email' => 'dolo@email.com',
+            'password' => 'secret'
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'success' => ['token', 'name']
+        ]);
+    }
+
+    public function testCategoryFetch()
+    {
+        $user = \App\User::find(1);
+
+        $response = $this->actingAs($user, 'api')
+            ->json('GET', '/api/category')
+            ->assertStatus(200)->assertJsonStructure([
+                    '*' => [
+                        'id',
+                        'name',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at'
+                    ]
+                ]
+            );
+    }
+
+    public function testCategoryCreation()
+    {
+        $this->withoutMiddleware();
+
+        $response = $this->json('POST', '/api/category', [
+            'name' => str_random(10),
+        ]);
+
+        $response->assertStatus(200)->assertJson([
+            'status' => true,
+            'message' => 'Category Created'
+        ]);
+    }
+    public function testCategoryDeletion()
+    {
+        $user = \App\User::find(1);
+
+        $category = \App\Category::create(['name' => 'To be deleted']);
+
+        $response = $this->actingAs($user, 'api')
+            ->json('DELETE', "/api/category/{$category->id}")
+            ->assertStatus(200)->assertJson([
+                'status' => true,
+                'message' => 'Category Deleted'
+            ]);
+    }
+}
