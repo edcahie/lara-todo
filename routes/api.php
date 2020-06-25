7af688677d4b3ecbdb16317fc09b87fc76baa947
Administrator <michel@enezaeducation.com>
2020-06-25 18:22:28 +0000

    Adding passport

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::resource('/task', 'TasksController');
+        Route::resource('/category', 'CategoryController');
+        Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+    });
